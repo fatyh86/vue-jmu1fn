05ad025e063cd38107733cb140f809adb4822b46
fatyh86 <fatyh_fth@yahoo.com>
2021-11-02 07:03:00 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-jmu1fn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-jmu1fn)
---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <div @click="test()">bbbb</div>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+var moment = require('moment');
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data: function() {
+    return: {
+      moment: moment,
+    }
+  },
+  methid: {
+    test: function(){
+      console.log('ssss');
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
